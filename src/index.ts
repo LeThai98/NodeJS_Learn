39dd20fd7de56adc9767be75d94fd6767a9cea2b
lethai <hongthaiit1998@gmail.com>
2024-05-10 05:41:39 +0700

    Build an Express Server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hello World!, Thai');
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/api', (req, res) => {
+    res.send('Hello, world!');
+});
+
+app.listen(port, ()=> {
+    console.log(`server started at localhost:${port}`)
+});
